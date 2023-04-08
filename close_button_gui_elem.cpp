477bcdb2e842619f574a914e7a35210a5ed191f6
Cylex9212 <USMANIFTIKHAR@cmail.carleton.ca>
2023-04-08 18:34:25 -0400

    Working Close Button
    LESGOOOO

--- close_button_gui_elem.cpp
+++ close_button_gui_elem.cpp
@@ -5,23 +5,29 @@ namespace game {
 
 	/* Gui inherits from Gui Elem */
 
-	CloseButtonGuiElem::CloseButtonGuiElem(Geometry* geom, Shader* shader, GLFWwindow* window) : ClickableGuiElem(geom, shader, window, glm::vec2(512,512), GameObject::textures.GetTexture("close_button")) {
+	CloseButtonGuiElem::CloseButtonGuiElem(Geometry* geom, Shader* shader, GLFWwindow* window) : ClickableGuiElem(geom, shader, window, glm::vec2(1,1), GameObject::textures.GetTexture("close_button")) {
 		type_ = ObjType::GUI_ELEM;
+
+		// Set Position to top right corner
+		SetPosition(glm::vec3(0.94f, 0.94f, 0.0f));
+		SetScale(glm::vec2(0.15, 0.15));
 	}
 
 
 	// Update function for moving the player object around
 	void CloseButtonGuiElem::Update(double delta_time, GuiState* gui_state) {
-
 		// Process input
 		ProcessInput();
 
 		// Call the parent's update method to move the object in standard way, if desired
-		GuiElem::Update(delta_time, gui_state);
+		ClickableGuiElem::Update(delta_time, gui_state);
 	}
 
 	void CloseButtonGuiElem::ProcessInput() {
-		if (glfwGetKey(window_, GLFW_KEY_Q) == GLFW_PRESS) { glfwSetWindowShouldClose(window_, true); }
+		// If clicked_ is true, and the user presses left click, close the window
+		if (clicked_ && glfwGetMouseButton(window_, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window_, true);
+		}
 	}
 
 } // namespace game
